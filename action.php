58a07275ff21d06aada2d28614a1a240bca18516
NayanAdhikary <113829617+NayanAdhikary@users.noreply.github.com>
2023-10-31 19:36:27 +0530

    update action.php

--- action.php
+++ action.php
@@ -335,7 +335,7 @@ if (isset($_POST["Common"])) {
 												</div>
 												<div class="product-body">
 													<h3 class="product-name"><a href="#">'.$product_title.'</a></h3>
-													<h4 class="product-price"><span class="qty">'.$n.'</span>$'.$product_price.'</h4>
+													<h4 class=""><span class="qty">'.$n.'</span>$'.$product_price.'</h4>
 												</div>
 												
 											</div>'
